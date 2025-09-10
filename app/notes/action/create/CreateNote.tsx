865282a669b92eafc63f850b9dfa2185f46c3b13
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2025-09-10 14:27:32 +0300

    Form

--- app/notes/action/create/CreateNote.tsx
+++ app/notes/action/create/CreateNote.tsx
@@ -1,13 +1,14 @@
 
 'use client';
 
+import css from './CreateNote.module.css'
 import NoteForm from "@/components/NoteForm/NoteForm";
 
 export default function CreateNoteClient() {
 
     return (
-        <div>
+        <main className={css.backdrop}>
             <NoteForm />
-        </div>
+        </main>
     );
 }
--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,140 +1,34 @@
-// "use client";
-
-// import css from './NoteForm.module.css';
-// import * as Yup from 'yup';
-// import { useId } from 'react';
-// import { Formik, Form, Field, FormikHelpers, ErrorMessage } from 'formik';
-
-// import { createNote } from '@/lib/api'
-// import { NoteFormType } from '@/types/note';
-// import { useMutation, useQueryClient } from '@tanstack/react-query';
-
-// const validationForm = Yup.object().shape({
-//     title: Yup.string()
-//         .min(3, "Title must be at least 3 characters")
-//         .max(50, "Content must be less than 50 characters")
-//         .required("Title is required"),
-//     content: Yup.string().max(500, "Content must be less than 500 characters"),
-//     tag: Yup.string()
-//         .oneOf(["Todo", "Work", "Personal", "Meeting", "Shopping"], "Invalid tag")
-//         .required("Tag is required"),
-// });
-
-// interface NoteFormProps {
-//     onSuccess: () => void,
-// }
-
-// const initialValues: NoteFormType = {
-//     title: "",
-//     content: "",
-//     tag: "",
-// }
-
-// export default function NoteForm({ onSuccess }: NoteFormProps) {
-//     const fieldId = useId();
-//     const queryClient = useQueryClient();
-
-//     const { mutate, isPending } = useMutation({
-//         mutationFn: (noteData: NoteFormType) => createNote(noteData),
-//         onSuccess: () => {
-//             queryClient.invalidateQueries({ queryKey: ["notes"] });
-//             onSuccess();
-//         },
-//     });
-
-//     const handleSubmit = (
-//         values: NoteFormType,
-//         FormikHelpers: FormikHelpers<NoteFormType>
-//     ) => {
-//         mutate(values);
-//         FormikHelpers.resetForm();
-//     }
-
-//     return (
-//         < Formik
-//             initialValues={initialValues}
-//             onSubmit={handleSubmit}
-//             validationSchema={validationForm}
-//         >
-//             <Form className={css.form}>
-//                 <div className={css.formGroup}>
-//                     <label htmlFor={`${fieldId}-title`}>Title</label>
-//                     <Field id={`${fieldId}-title`} type="text" name="title" className={css.input} />
-//                     <ErrorMessage
-//                         name="title"
-//                         component="span"
-//                         className={css.error}
-//                     />
-//                 </div>
-
-//                 <div className={css.formGroup}>
-//                     <label htmlFor={`${fieldId}-content`}>Content</label>
-//                     <Field
-//                         as="textarea"
-//                         id={`${fieldId}-content`}
-//                         name="content"
-//                         rows={8}
-//                         className={css.textarea}
-//                     />
-//                     <ErrorMessage
-//                         name="content"
-//                         component="span"
-//                         className={css.error}
-//                     />
-//                 </div>
-
-//                 <div className={css.formGroup}>
-//                     <label htmlFor={`${fieldId}-tag`}>Tag</label>
-//                     <Field
-//                         as="select"
-//                         id={`${fieldId}-tag`}
-//                         name="tag"
-//                         className={css.select}>
-//                         <option value="Todo">Todo</option>
-//                         <option value="Work">Work</option>
-//                         <option value="Personal">Personal</option>
-//                         <option value="Meeting">Meeting</option>
-//                         <option value="Shopping">Shopping</option>
-//                     </Field>
-//                     <ErrorMessage
-//                         name="tag"
-//                         component="span"
-//                         className={css.error}
-//                     />
-//                 </div>
-
-//                 <div className={css.actions}>
-//                     <button onClick={onSuccess} type="button" className={css.cancelButton}>
-//                         Cancel
-//                     </button>
-//                     <button
-//                         type="submit"
-//                         className={css.submitButton}
-//                         disabled={isPending}
-//                     >
-//                         Create note
-//                     </button>
-//                 </div>
-//             </Form>
-//         </Formik >
-//     )
-// }
 
 'use client';
 
 import css from './NoteForm.module.css';
+import * as Yup from 'yup';
 
-import { useId } from 'react';
+import { useId, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
+import { useStore } from '@/lib/store/noteStore';
+
 import { createNote } from '@/lib/api';
 import { NoteFormType } from '@/types/note';
-import { useStore } from '@/lib/store/noteStore';
+
+const validationForm = Yup.object().shape({
+    title: Yup.string()
+        .min(3, "Title must be at least 3 characters")
+        .max(50, "Content must be less than 50 characters")
+        .required("Title is required"),
+    content: Yup.string().max(500, "Content must be less than 500 characters"),
+    tag: Yup.string()
+        .oneOf(["Todo", "Work", "Personal", "Meeting", "Shopping"], "Invalid tag")
+        .required("Tag is required"),
+});
 
 export default function NoteForm() {
     const fieldId = useId();
     const router = useRouter();
+
     const { draft, setDraft, clearDraft } = useStore();
+    const [errors, setErrors] = useState<Partial<Record<keyof NoteFormType, string>>>({});
 
     const mutation = useMutation({
         mutationFn: (noteData: NoteFormType) => createNote(noteData),
@@ -144,53 +38,81 @@ export default function NoteForm() {
         },
     });
 
-    const handleSubmit = () => {
-        mutation.mutate(draft);
+    const handleSubmit = async () => {
+        try {
+            await validationForm.validate(draft, { abortEarly: false });
+            setErrors({});
+            mutation.mutate(draft);
+        } catch (error) {
+            if (error instanceof Yup.ValidationError) {
+                const formattedErrors: Partial<Record<keyof NoteFormType, string>> = {};
+                error.inner.forEach((error) => {
+                    if (error.path) {
+                        formattedErrors[error.path as keyof NoteFormType] = error.message;
+                    }
+                });
+                setErrors(formattedErrors);
+            }
+        }
     };
 
     return (
-        <div className={css.form}>
-            <form action={handleSubmit}>
-                <div className={css.formGroup}>
-                    <label htmlFor={`${fieldId}-title`}>Title</label>
-                    <input
-                        type="text"
-                        name="title"
-                        value={draft.title}
-                        id={`${fieldId}-title`}
-                        onChange={(e) => setDraft({ title: e.target.value })}
-                        required
-                    />
-                </div>
+        <div className={css.main} >
+            <div className={css.form}>
+                <form action={handleSubmit} >
+                    <div className={css.formGroup}>
+                        <label htmlFor={`${fieldId}-title`}>Title</label>
+                        <input
+                            type="text"
+                            name="title"
+                            className={css.input}
+                            value={draft.title}
+                            id={`${fieldId}-title`}
+                            onChange={(e) => setDraft({ title: e.target.value })}
+                            required
+                        />
+                        {errors.title && <span className={css.error}>{errors.title}</span>}
+                    </div>
 
-                <div className={css.formGroup}>
-                    <textarea
-                        name="content"
-                        value={draft.content}
-                        onChange={(e) => setDraft({ content: e.target.value })}
-                        required
-                    />
-                </div>
+                    <div className={css.formGroup}>
+                        <label htmlFor={`${fieldId}-content`}>Content</label>
+                        <textarea
+                            name="content"
+                            className={css.textarea}
+                            value={draft.content}
+                            id={`${fieldId}-content`}
+                            onChange={(e) => setDraft({ content: e.target.value })}
+                            required
+                        />
+                        {errors.content && <span className={css.error}>{errors.content}</span>}
+                    </div>
 
-                <div className={css.formGroup}>
-                    <select name="tag" value={draft.tag} onChange={(e) => setDraft({ tag: e.target.value as typeof draft['tag'] })}
+                    <div className={css.formGroup}>
+                        <label htmlFor={`${fieldId}-tag`}>Tag</label>
+                        <select
+                            name="tag"
+                            className={css.select}
+                            value={draft.tag}
+                            id={`${fieldId}-tag`}
+                            onChange={(e) => setDraft({ tag: e.target.value as typeof draft['tag'] })}
+                            required>
+                            <option value="Todo">Todo</option>
+                            <option value="Work">Work</option>
+                            <option value="Personal">Personal</option>
+                            <option value="Meeting">Meeting</option>
+                            <option value="Shopping">Shopping</option>
+                        </select>
+                        {errors.tag && <span className={css.error}>{errors.tag}</span>}
+                    </div>
 
-                        required>
-                        <option value="Todo">Todo</option>
-                        <option value="Work">Work</option>
-                        <option value="Personal">Personal</option>
-                        <option value="Meeting">Meeting</option>
-                        <option value="Shopping">Shopping</option>
-                    </select>
-                </div>
-
-                <div className={css.actions}>
-                    <button className={css.submitButton} type="submit">Create note</button>
-                    <button className={css.cancelButton} type="button" onClick={() => router.push('/notes/filter/All')}>
-                        Cancel
-                    </button>
-                </div>
-            </form>
+                    <div className={css.actions}>
+                        <button className={css.submitButton} type="submit">Create note</button>
+                        <button className={css.cancelButton} type="button" onClick={() => router.push('/notes/filter/All')}>
+                            Cancel
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
